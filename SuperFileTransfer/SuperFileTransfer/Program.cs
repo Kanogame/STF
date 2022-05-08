9e7525695b653b3545e3f8796f229e1fa9a9d898
kanogame <wasdqert1222@gmail.com>
2022-05-08 19:45:04 +0700

    first commit

---
+++ SuperFileTransfer/SuperFileTransfer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuperFileTransfer
+{
+    class Program
+    {
+
+        List<computer> computers;
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.main();
+
+        }
+
+        Program()
+        {
+            computers = new List<computer>();
+        }
+
+        void main()
+        {
+            var acceptClientThreads = new Thread(AcceptClients);
+            var signalsToClientsThread = new Thread(SignalToClients);
+            acceptClientThreads.IsBackground = true;
+            acceptClientThreads.Start();
+            Console.ReadKey();
+        }
+
+        void AcceptClients()
+        {
+            var listener = new TcpListener(IPAddress.Any, 13531);
+            listener.Start();
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                addClient(client);
+            }
+        }
+
+        void addClient(TcpClient client)
+        {
+            var ns = client.GetStream();
+            bool requestsToServer = ns.ReadByte() == 1;
+            Console.WriteLine(client.Client.RemoteEndPoint.ToString());
+            if (requestsToServer)
+            {
+                Console.WriteLine("Запросы на сервер");
+            }
+            else
+            {
+                Console.WriteLine("Запросы к клиенту");
+            }
+        }
+
+        void SignalToClients()
+        {
+
+        }
+    }
+}
---
+++ SuperFileTransferClient/SuperFileTransferClient/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperFileTransferClient
+{
+    public partial class MainForm : Form
+    {
+
+        TcpClient sender;
+        TcpClient retriever;
+        NetworkStream senderStream;
+        NetworkStream retrieverStream;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object senderIGNORE, EventArgs e)
+        {
+            string server = "localhost";
+            sender = new TcpClient();
+            sender.Connect(server, 13531);
+            senderStream = sender.GetStream();
+            senderStream.WriteByte(1);
+
+
+            retriever = new TcpClient();
+            retriever.Connect(server, 13531);
+            retrieverStream = retriever.GetStream();
+            retrieverStream.WriteByte(0);
+        }
+    }
+}
